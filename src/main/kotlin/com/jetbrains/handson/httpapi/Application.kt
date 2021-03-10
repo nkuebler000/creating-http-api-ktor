57541bf3ac181d8fcb5e01ef77e3311b127cd4cd
Nick Kuebler <nick@subsonic.us>
2021-03-09 19:54:40 -0800

    finish tutorial

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.handson.httpapi
 
 import com.jetbrains.handson.httpapi.routes.registerCustomerRoutes
+import com.jetbrains.handson.httpapi.routes.registerOrderRoutes
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.serialization.*
@@ -8,9 +9,10 @@ import io.ktor.serialization.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-fun Application.module() {
+fun Application.module(testing: Boolean = true) {
     install(ContentNegotiation) {
         json()
     }
     registerCustomerRoutes()
+    registerOrderRoutes()
 }
